7732a989007cc48bc791500b3a6a200c4c2ea45b
Max Zhao <alcasa.mz@gmail.com>
2016-04-07 11:40:50 +0200

    add opening script

---
+++ Goodies/urxvt-open.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# very simple solution, since urxvt is very nice about window titles
+# wmctrl will simply switch to first window with the corresponding name in the title
+STATUS=$(wmctrl -l | grep urxvt)
+echo $STATUS
+if [ "$STATUS" ];
+then
+	wmctrl -a urxvt
+else
+	urxvt
+fi
