88224f50618962c0f459b6a2fdd094ee47b18ae5
Max Zhao <alcasa.mz@gmail.com>
2016-04-08 21:28:59 +0200

    fixing urxvt open

--- Goodies/urxvt-open.sh
+++ Goodies/urxvt-open.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # very simple solution, since urxvt is very nice about window titles
 # wmctrl will simply switch to first window with the corresponding name in the title
-STATUS=$(wmctrl -l | grep urxvt.URxvt)
+STATUS=$(wmctrl -lx | grep urxvt.URxvt)
 echo $STATUS
 if [ "$STATUS" ];
 then
-	wmctrl -a urxvt
+	wmctrl -ax urxvt.URxvt
 else
 	urxvt
 fi
