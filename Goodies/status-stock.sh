5844ee124bb76ec8bbae23f4d750604fd50509c8
Max Zhao <alcasa.mz@gmail.com>
2016-06-11 14:29:47 +0200

    mess

--- Goodies/status-stock.sh
+++ Goodies/status-stock.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-# requires fontawesome for icons
 # get first temperature from coretemp
 temp=$(sensors -u coretemp-isa-0000 | grep -i -m 1 input | grep -o "[^ ]*$")
 
@@ -16,5 +15,5 @@ m5=$(awk '{print $2}' <<< $load)
 m15=$(awk '{print $3}' <<< $load)
 
 # combine into status string
-line="<span font_weight='bold'><span fgcolor='#D0B03C' font='FontAwesome' font_weight='normal'> </span> <span fgcolor='#FFE377'>$m1 $m5 $m15</span><span fgcolor='#72B3CC' font='FontAwesome' font_weight='normal'>   </span> <span fgcolor='#9CD9F0'>${used}M used</span> <span fgcolor='#C75646' font='FontAwesome' font_weight='normal'>  </span> <span fgcolor='#E09690'>${temp%.*}°C</span></span>"
+line="<span font_weight='bold'><span fgcolor='#D0B03C'>CPU<span font='FontAwesome' font_weight='normal'></span></span> <span fgcolor='#FFE377'>$m1 $m5 $m15</span><span fgcolor='#72B3CC'>  RAM</span> <span fgcolor='#9CD9F0'>${used}M used</span> <span fgcolor='#C75646'>Temp</span> <span fgcolor='#E09690'>${temp%.*}°C</span></span>"
 echo "<txt>$line</txt>"
