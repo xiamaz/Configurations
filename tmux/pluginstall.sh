521c5afed81a33873dbb7f7415bb2ad463f6beee
Max Zhao <alcasa.mz@gmail.com>
2016-12-09 11:03:01 +0100

    adding a better tmux configuration

---
+++ tmux/pluginstall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# simple script for installing tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Issue C-a I to install packages defined in .tmux.conf."
