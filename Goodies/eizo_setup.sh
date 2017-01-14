991f9e309310afa334ea284d7152f53b15deacc0
Max Zhao <alcasa.mz@gmail.com>
2017-01-14 12:37:51 +0100

    setting new default resolution

--- Goodies/eizo_setup.sh
+++ Goodies/eizo_setup.sh
@@ -2,17 +2,17 @@
 # script for adding undetected resolutions to our eizo crt monitor
 displayName="$1"
 
-
-mode6="1400x1050_85.00" 
-xrandr --newmode "$mode6" 179.50  1400 1512 1656 1912  1050 1053 1057 1105 -hsync +vsync
-xrandr --addmode $displayName $mode6
-
 # first add the most important primary mode
 # 1280x1024 is supposed to be the default resolution, but the ratio is stretched, let us use
 # 1280x960 instead
 mode1="1280x960_95.00"
 xrandr --newmode $mode1  168.25  1280 1376 1512 1744  960 963 967 1017 -hsync +vsync
 xrandr --addmode $displayName $mode1
+
+mode6="1400x1050_85.00" 
+xrandr --newmode "$mode6" 179.50  1400 1512 1656 1912  1050 1053 1057 1105 -hsync +vsync
+xrandr --addmode $displayName $mode6
+
 
 # try to add other diplay modes automatically
 
