c4fd75d92b90bdb1f61ab8bdc982d1fd2558d8c5
Max Zhao <alcasa.mz@gmail.com>
2017-03-12 18:29:19 +0100

    adding tooltip for dpmstoggle

--- XFCE/Genmon/dpmstoggle.sh
+++ XFCE/Genmon/dpmstoggle.sh
@@ -3,8 +3,10 @@ presentation_mode=$(xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/
 
 if $presentation_mode; then
 	echo "<img>/home/max/SystemSettings/dpms/teahot.png</img>
+	<tool>Presentation mode on</tool>
 	<click>$HOME/SystemSettings/dpms/tog.sh</click>"
 else
 	echo "<img>/home/max/SystemSettings/dpms/teacold.png</img>
+	<tool>Presentation mode off</tool>
 	<click>$HOME/SystemSettings/dpms/tog.sh</click>"
 fi
