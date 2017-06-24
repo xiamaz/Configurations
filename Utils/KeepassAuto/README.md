76b5dcbfd3f542d48f2c0c6011288ddef48d0d3a
Max Zhao <alcasa.mz@gmail.com>
2017-06-24 13:55:06 +0200

    adding keepass

---
+++ Utils/KeepassAuto/README.md
@@ -0,0 +1,2 @@
+# Keepass Gnome Keyring unlock
+For setup add a key with keepass2 as name.
--- Utils/KeepassAuto/keepass.sh
+++ Utils/KeepassAuto/keepass.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-/home/max/SystemSettings/KeepassAutostart/kr.py get keepass2 | keepass -pw-stdin -keyfile:/home/max/Documents/keepass.key /home/max/Dropbox/shinier.kdbx
+kpcmd="keepassxc"
+dbLocation="/home/max/Sync/Files/shinier.kdbx"
+keyLocation="/home/max/Documents/keepass.key"
+/home/max/SystemSettings/KeepassAutostart/kr.py get keepass2 | $kpcmd -pw-stdin -keyfile:$keyLocation $dbLocation
