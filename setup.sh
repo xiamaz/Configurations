ced0ca7b93044a17c7364cda755c378bff11f168
Max Zhao <alcasa.mz@gmail.com>
2017-06-24 13:50:06 +0200

    start to write a setup file

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+### Quick Configurations Setup
+
+## detect operating system
+# aim for debian and archlinux support
+
+
+## install missing packages
+
+## link and copy files into place
