11ef8edc148ed124632ace00b40877e24edb4f15
Max Zhao <alcasa.mz@gmail.com>
2017-06-29 01:09:00 +0200

    modifying opening scripts

--- Goodies/st-open.sh
+++ Goodies/st-open.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # very simple solution, since urxvt is very nice about window titles
 # wmctrl will simply switch to first window with the corresponding name in the title
-STATUS=$(wmctrl -lx | grep urxvt.URxvt)
+STATUS=$(wmctrl -lx | grep st-256color.st-256color)
 echo $STATUS
 if [ "$STATUS" ];
 then
-	wmctrl -a urxvt.URxvt -x
+	wmctrl -a st-256color.st-256color -x
 else
-	urxvt -e zsh -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER"
+	st -e zsh -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER"
 fi
