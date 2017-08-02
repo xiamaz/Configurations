ef4728b8686c8afe59b05e9edf6400409691e57c
Max Zhao <alcasa.mz@gmail.com>
2017-08-02 23:36:22 +0200

    let st use dash!

--- Goodies/st-open.sh
+++ Goodies/st-open.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/dash
 # very simple solution, since urxvt is very nice about window titles
 # wmctrl will simply switch to first window with the corresponding name in the title
 STATUS=$(wmctrl -lx | grep st-256color.st-256color)
@@ -7,5 +7,5 @@ if [ "$STATUS" ];
 then
 	wmctrl -a st-256color.st-256color -x
 else
-	st -e zsh -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER"
+	st -e dash -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER"
 fi
