b3d56a4c992ecf0709e98c190c4859d8cea53881
Max Zhao <alcasa.mz@gmail.com>
2017-10-15 22:37:43 +0200

    polybar dynamically add bars to new monitors

--- bspwm/polybar/launch.sh
+++ bspwm/polybar/launch.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/sh
+#!/bin/sh
+MAIN_MONITOR='eDP1'
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,10 +7,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-MONITOR=DP-3 polybar top &
-MONITOR=DP-3 polybar bottom &
-MONITOR=DVI-I-1 polybar top &
-MONITOR=DVI-I-1 polybar bottom &
+# Launch internal laptop bar and external monitor bars with less information
+for monitor in "$( bspc query -M --names )"; do
+	echo $monitor
+	if [ "$monitor" == $MAIN_MONITOR ]; then
+	 	MONITOR=$monitor polybar main &
+	else
+	 	MONITOR=$monitor polybar side &
+	fi
+done
 
 echo "Bars launched..."
