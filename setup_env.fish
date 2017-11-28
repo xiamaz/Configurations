12736ff641e25924aaac03e080570bb87bf92cb1
Max Zhao <alcasa.mz@gmail.com>
2017-11-28 12:03:43 +0100

    some automation for neovim

--- setup_env.fish
+++ setup_env.fish
@@ -4,3 +4,13 @@ echo (pwd)
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 
 fisher edc/bass
+
+# setup neovim
+mkdir -p $HOME/Git
+cd $HOME/Git
+git clone https://github.com/xiamaz/Configurations.git
+
+ln -s $HOME/Git/Configurations/Vim/vim $HOME/.config/nvim
+touch $HOME/.vimrc.local
+cp $HOME/Git/Configurations/Dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+sh $HOME/Git/Configurations/Dotfiles/tmux/pluginstall.sh
