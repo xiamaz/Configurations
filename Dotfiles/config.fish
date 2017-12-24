70f4e3f8950ee3ec13691f511d1083849cd1103c
Max Zhao <alcasa.mz@gmail.com>
2017-12-25 00:03:50 +0100

    adding dotfile changes

--- Dotfiles/config.fish
+++ Dotfiles/config.fish
@@ -2,4 +2,5 @@ bass source $HOME/.zprofile
 # aliases for python virtualenvs
 alias pmk 'python3 -m venv env'
 alias pa '. env/bin/activate.fish'
-alias pd 'pip install -r requirements.txt'
+alias pdep 'pip install -r requirements.txt'
+alias pd 'deactivate'
