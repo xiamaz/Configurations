32402ca909a1aef5d0aae8e64b14a9f455737120
Max Zhao <alcasa.mz@gmail.com>
2018-04-15 10:21:46 +0200

    Updating fish configuration file

--- Dotfiles/config.fish
+++ Dotfiles/config.fish
@@ -4,3 +4,9 @@ alias pmk 'python3 -m venv env'
 alias pa '. env/bin/activate.fish'
 alias pdep 'pip install -r requirements.txt'
 alias pd 'deactivate'
+
+# completion for fish
+complete -c pacman -n "__fish_contains_opt -s U upgrade" -xa '(__fish_complete_suffix pkg.tar)' -d 'Package file'
+
+set -x -U GOPATH $HOME/go
+set -x PATH $GOPATH/bin $PATH
