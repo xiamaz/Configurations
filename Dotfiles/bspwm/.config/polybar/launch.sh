d2da9605a3dd29d4a77582c5f3ff497f775c8dc7
Max Zhao <alcasa.mz@gmail.com>
2018-04-19 08:43:41 +0200

    Updating polybar launch script

--- Dotfiles/bspwm/.config/polybar/launch.sh
+++ Dotfiles/bspwm/.config/polybar/launch.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-MAIN_MONITOR='eDP1'
+internal="eDP1"
+if [ $1 ]; then
+	MAIN_MONITOR="$1"
+else
+	MAIN_MONITOR="$internal"
+fi
 
 # Terminate already running bar instances
 killall -q polybar
@@ -9,7 +14,6 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch internal laptop bar and external monitor bars with less information
 for monitor in $( bspc query -M --names ); do
-	echo $monitor
 	if [ $monitor = "$MAIN_MONITOR" ]; then
 	 	MONITOR=$monitor polybar main &
 	else
