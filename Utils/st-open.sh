b8d7f445e50e5c4daa1f2d85a1ac2875551978fb
Max Zhao <alcasa.mz@gmail.com>
2018-04-21 20:30:00 +0200

    Remove Dash weirdness

--- Utils/st-open.sh
+++ Utils/st-open.sh
@@ -1,4 +1,4 @@
-#!/bin/dash
+#!/bin/sh
 # very simple solution, since urxvt is very nice about window titles
 # wmctrl will simply switch to first window with the corresponding name in the title
 STATUS=$(wmctrl -lx | grep st-256color.st-256color)
@@ -7,5 +7,5 @@ if [ "$STATUS" ];
 then
 	wmctrl -a st-256color.st-256color -x
 else
-	st -e dash -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER"
+	st -e sh -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER"
 fi
