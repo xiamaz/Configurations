969d31aaf67db02b909b1a8ec79d83846dca7ef3
Max Zhao <alcasa.mz@gmail.com>
2018-05-03 18:48:43 +0200

    Simpliying tmux startup

--- Dotfiles/bspwm/bspwm/.local/bin/term-open.fish
+++ Dotfiles/bspwm/bspwm/.local/bin/term-open.fish
@@ -1,12 +1,12 @@
-#!/bin/fish
+#!/usr/bin/fish
 
 function create_st
 	st -t $argv[1] -e sh -c $argv[2]
 end
 
 function raise_or_create
-	set $cmd $argv[2]
-	set $window_title $argv[1]
+	set cmd $argv[2]
+	set window_title $argv[1]
 	if test (wmctrl -l | grep $window_title)
 		wmctrl -a $window_title -F
 	else
@@ -16,10 +16,10 @@ end
 
 switch $argv[1]
 case "main"
-	raise_or_create "st-main-local-tmux" "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER"
+	raise_or_create "st-main-local-tmux" "tmux a"
 case "single"
 	create_st "st" "tmux new-session"
 case "home"
-	raise_or_create "st-main-home-tmux" "mosh home -- tmux -q has-session && tmux attach-session -d || tmux new-session -n$USER -s$USER"
+	raise_or_create "st-main-home-tmux" "mosh home -- tmux a"
 end
 
