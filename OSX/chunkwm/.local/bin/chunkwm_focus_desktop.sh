e5b254bf3c5c072450141a3184f4d4ddc83e91fb
Max Zhao <alcasa.mz@gmail.com>
2019-02-09 20:54:17 +0100

    Adding chunkwm configuration

---
+++ OSX/chunkwm/.local/bin/chunkwm_focus_desktop.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -z $1 ]; then exit; fi
+# Focus the nth desktop on the given monitor
+monitor_id=$(chunkc tiling::query --monitor id)
+selected_desktop_id=$(chunkc tiling::query --desktops-for-monitor $monitor_id | cut -d " " -f $1)
+case $selected_desktop_id in
+	*[[:alnum:]]*)
+		chunkc tiling::desktop --focus $selected_desktop_id
+		;;
+esac
