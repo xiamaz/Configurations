894a6d425cc6a76854d34a1a33e6c4e4f5177c2a
Max Zhao <alcasa.mz@gmail.com>
2019-08-08 14:58:21 +0200

    Removing useless echo

--- authorize_ssh.sh
+++ authorize_ssh.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 host=$1
 ssh_key=$(cat ~/.ssh/id_rsa.pub)
-echo $ssh_key
 ssh $host "touch ~/.ssh/authorized_keys;if ! grep \"$ssh_key\" ~/.ssh/authorized_keys > /dev/null; then echo $ssh_key >> ~/.ssh/authorized_keys; fi"
