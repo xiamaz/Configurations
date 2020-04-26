400b96732c133e365cab2e33f537f26ab4bd01b6
Max Zhao <alcasa.mz@gmail.com>
2020-04-26 15:26:13 +0200

    Speed optimizations for window switching scripts

--- OSX/yabai-switch-window.sh
+++ OSX/yabai-switch-window.sh
@@ -2,10 +2,6 @@
 TARGET_INDEX=$1
 
 SPACE_QUERY=$(yabai -m query --spaces)
-CUR_SPACE=$(jq '.[] | select(.focused == 1).index' <<EOF
-$SPACE_QUERY
-EOF
-)
 CUR_DISPLAY=$(jq '.[] | select(.focused == 1).display' <<EOF
 $SPACE_QUERY
 EOF
@@ -19,10 +15,7 @@ EOF
 if [ -z "$TARGET_SPACE_INDEX" ]; then
 	echo "EE: Index does not exist on current monitor ${CUR_DISPLAY}."
 	exit 1
-elif [ $TARGET_SPACE_INDEX -ne $CUR_SPACE ]; then
-	echo "Switching to space index ${TARGET_SPACE_INDEX}"
-	yabai -m space --focus $TARGET_SPACE_INDEX
 else
-	echo "Switching to most recent space."
-	yabai -m space --focus recent
+	echo "Moving window to space index ${TARGET_SPACE_INDEX}"
+	yabai -m window --space $TARGET_SPACE_INDEX
 fi
